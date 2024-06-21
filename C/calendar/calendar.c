96cf8ce54faa68048e84f5d2b7aca42992019b90
jyanev <jyanev1@gmail.com>
2024-06-21 03:40:41 -0500

    updated calendar project

--- C/calendar/calendar.c
+++ C/calendar/calendar.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
+#include <time.h>
 
-#define TRUE     1
-#define FALSE    0
+
+#define TRUE               1
+#define FALSE              0
+#define MAX_NOTE_LENGTH   255
 
 
 int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 char* weekdays[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 char* months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+char** notes;
+int size;
 
-int input_year(void) {
-    int year;
-	
-	printf("Please enter a year (example: 1999) : ");
-	scanf("%d", &year);
 
-	return year; 
+void allocate_new_note_mem(int new_size) {
+    size = new_size;
+    notes = (char**)realloc(notes, (size * 12 * 31) * sizeof(char*));
+
+    for (int i=0 ; i < (size * 12 * 31) ; i++) {
+        notes[i] = (char*)malloc((MAX_NOTE_LENGTH + 1) * sizeof(char));
+        sprintf(notes[i], "");
+    }
 }
+
 
 void is_leap_year(int year) {
     if(year % 4 == FALSE && year % 100 != FALSE || year % 400 == FALSE) {
@@ -27,6 +34,7 @@ void is_leap_year(int year) {
 	    days_in_month[1] = 28;
     }
 }
+
 
 int calculate_first_day_of_month(int month, int year) {
     int day;
@@ -39,11 +47,13 @@ int calculate_first_day_of_month(int month, int year) {
     if (year > 1582) {
         day = ((((13*(month+1))/5 + (year%100) + (year%100)/4 + year/400 + 5*(year/100) + 1) % 7) + 6) % 7;
     } else {
-        printf("  *** JULIAN CALENDAR NOT IMPLEMENTED ***  ");
-        // use Julian calculator
+        printf("  *** tf you doing back here??? your great great great grandmother hadnt even been born yet ***  ");
+        free (notes);
+        exit(0);
     }
     return day;
 }
+
 
 int print_month_info(int weekday, int month, int year) {
     
@@ -66,15 +76,22 @@ int print_month_info(int weekday, int month, int year) {
         } else {
             firstweek = 0;
         }
+
+        if (strcmp(notes[(year-1582) * (month+1) * day - 1], "") != 0) {
+            printf("\033[0;31m");
+        } 
+
         if (day < 10) {
             printf("%d    ", day);
         } else {
             printf("%d   ", day);
         }
         weekday = (weekday + 1) % 7;
+        printf("\033[0m");
     }
     return weekday;
 }
+
 
 void process_user_input(int day, int month, int year) {
 
@@ -82,11 +99,12 @@ void process_user_input(int day, int month, int year) {
     
     printf("\n\n\n");
     printf("\033[0;33m");
-    printf("Press 'd' for Next Month, 'a' for Previous Month, and 'q' to Quit.\nRed date indicates a note added to  date. Press 'x' to view note details: ");
+    printf("Press 'd' for Next Month, 'a' for Previous Month, and 'q' to Quit.\nRed date indicates a note added to date. Press 'x' to choose date: ");
     printf("\033[0m");
 
     int user_input;
-    user_input = getch();
+    user_input = getchar();
+    fflush(stdin);
     printf("\n\n\n");
 
     switch (user_input) {
@@ -96,8 +114,12 @@ void process_user_input(int day, int month, int year) {
                 year++;
                 month = 0;
                 is_leap_year(year);
+                if (year - 1582 > size) {
+                    allocate_new_note_mem(size + 50);
+                }
             }
             process_user_input(day, month, year);
+
         case 'a':
             month--;
             if (month == -1) {
@@ -107,27 +129,125 @@ void process_user_input(int day, int month, int year) {
             }
             day = calculate_first_day_of_month(month, year);
             process_user_input(day, month, year);
+
         case 'x':
-            printf("  **NOT IMPLEMENTED**  ");
+            int d;
+            printf("Which date would you like to view? : ");
+            char temp_day[2];
+            fgets(temp_day, 3, stdin);
+            d = atoi(temp_day);
+            fflush(stdin);
+            printf("\n\nViewing day: %d\n\n", d);
+
+            if (strcmp(notes[(year-1582) * (month+1) * d - 1], "") != 0) {
+                printf("Note:\n-----\n%s\n\n", notes[(year-1582) * (month+1) * d - 1]);
+
+                printf("Would you like to replace this note? (y/n) : ");
+                user_input = getchar();
+                fflush(stdin);
+                printf("\n\n");
+                if (user_input == 'y') {
+                    printf("New note (max %d characters): ", MAX_NOTE_LENGTH);
+                    char buffer[MAX_NOTE_LENGTH];
+                    fgets(buffer, MAX_NOTE_LENGTH+1, stdin);
+                    buffer[strcspn(buffer, "\n")] = 0;
+                    notes[(year-1582) * (month+1) * d - 1] = buffer;
+                } else {
+                    printf("Would you like to remove this note? (y/n) : ");
+                    user_input = getchar();
+                    fflush(stdin);
+                    printf("\n\n");
+                    if (user_input == 'y') {
+                        notes[(year-1582) * (month+1) * d - 1] = "";
+                    }
+                }
+
+            } else {
+                printf("No notes\n\n");
+                printf("Would you like to add a note? (y/n) : ");
+                user_input = getchar();
+                fflush(stdin);
+                printf("\n\n");
+                if (user_input == 'y') {
+                    printf("New note (max %d characters) : ", MAX_NOTE_LENGTH);
+                    char buffer[MAX_NOTE_LENGTH];
+                    fgets(buffer, MAX_NOTE_LENGTH+1, stdin);
+                    buffer[strcspn(buffer, "\n")] = 0;
+                    notes[(year-1582) * (month+1) * d - 1] = buffer;
+                }
+            }
+
+            printf("\n\n\n");
             day = calculate_first_day_of_month(month, year);
             process_user_input(day, month, year);
+        
         case 'q':
-            printf("Closing...");
+            printf("Saving and Closing...");
+            FILE *save_file;
+            save_file = fopen("notes.txt","w");
+            char* reset = NULL;
+            fwrite(reset, 1, 1, save_file);
+            fclose(save_file);
+            
+            save_file = fopen("notes.txt","wb");
+
+            for (int i=0 ; i < (size * 12 * 31) ; i++) {
+                fprintf(save_file, "%s,", notes[i]);
+            }
+            fclose(save_file);
+            free (notes);
             exit(0);
+
         default:
             printf("  **INVALID INPUT**  ");
             day = calculate_first_day_of_month(month, year);
             process_user_input(day, month, year);
-            
     }
 }
 
-int main(void) {
+
+int main() {
     
-    int year = input_year();
-    int month = 0;
-    int firstDay = calculate_first_day_of_month(month, year);
+    time_t current_time = time(NULL);
+    struct tm time;
+    time = *gmtime(&current_time);
+
+    int month = time.tm_mon;
+    int year = time.tm_year + 1900;
+
+    int first_day = calculate_first_day_of_month(month, year);
     is_leap_year(year);
-    process_user_input(firstDay, month, year);
+
+    size = (year-1582);
+
+    notes = (char**)malloc((size * 12 * 31) * sizeof(char*));
+
+    FILE *save_file;
+    save_file = fopen("notes.txt","r");
+
+    for (int i=0 ; i < (size * 12 * 31) ; i++) {
+        notes[i] = (char*)malloc((MAX_NOTE_LENGTH + 1) * sizeof(char));
+        sprintf(notes[i], "");
+    }
+
+    char buffer[MAX_NOTE_LENGTH+1]; 
+    int result;
+    int i = 0;
+    do {
+        result = fscanf(save_file, "%[^,]", buffer);
+
+        if(result == 0) {
+            i++;
+            result = fscanf(save_file, "%*c");
+        } else {
+            if (result != EOF) 
+                notes[i] = buffer;
+        }
+
+    } while(result != EOF);
+
+    
+    
+    process_user_input(first_day, month, year);
     
 }
